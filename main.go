0b1705f27ecad46d81b95b22d9b6c9c5b0ffb1bb
Av1d <av1d1ty@pm.me>
2024-03-04 09:50:43 +0200

    add chirp validation

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -14,8 +16,13 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
 	mux.HandleFunc("GET /api/reset", apiCfg.resetHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 	corsMux := middlewareCors(mux)
 	log.Fatal(http.ListenAndServe(":8080", corsMux))
+}
+
+type apiConfig struct {
+	fileserverHits int
 }
 
 func middlewareCors(next http.Handler) http.Handler {
@@ -37,10 +44,6 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
-type apiConfig struct {
-	fileserverHits int
-}
-
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits++
@@ -51,7 +54,7 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-    page := `
+	page := `
     <html>
     <body>
         <h1>Welcome, Chirpy Admin</h1>
@@ -72,4 +75,73 @@ func middlewareLog(next http.Handler) http.Handler {
 		log.Printf("%s %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Body string `json:"body"`
+	}
+	type response struct {
+		CleanedBody string `json:"cleaned_body"`
+		// Error string `json:"error"`
+	}
+	chirpVal := chirp{}
+	err := json.NewDecoder(r.Body).Decode(&chirpVal)
+	switch {
+	case err != nil:
+        respondWithError(w, http.StatusInternalServerError,
+                            fmt.Sprintf("Error decoding parameters: %s", err))
+	case len(chirpVal.Body) > 140:
+        respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+	default:
+        resp := response{CleanedBody: censorProfanity(chirpVal.Body)}
+        respJSON, _ := json.Marshal(resp)
+		w.WriteHeader(200)
+        w.Header().Set("Content-Type", "application/json")
+        w.Write([]byte(respJSON))
+	}
+}
+
+func censorProfanity(body string) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		if isProfanity(strings.ToLower(word)) {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func isProfanity(word string) bool {
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+	for _, profane := range profaneWords {
+		if word == profane {
+			return true
+		}
+	}
+	return false
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
 }
